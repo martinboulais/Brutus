a198ed3a2df9ce167ea758444715a0e4991d6850
Quentin Picault <qpicault@yahoo.com>
2020-01-07 17:58:45 +0100

    Notification fix main

--- app/src/main/java/com/cpe/brutus/MainActivity.java
+++ app/src/main/java/com/cpe/brutus/MainActivity.java
@@ -1,14 +1,30 @@
 package com.cpe.brutus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.cpe.brutus.services.AlarmReceiver;
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
+        notificationIntent.putExtra("titleText", "Va courir gros lard !");
+        notificationIntent.putExtra("contentText", "Découvre ton nouveau challenge.");
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 5);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
     }
 }
